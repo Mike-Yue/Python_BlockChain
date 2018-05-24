eed7a3e20515a65b9668830ea1aaafba8ae3f138
Mike <mikeyue97@gmail.com>
2018-05-24 16:52:30 -0700

    Added beginnings of GUI

--- source.py
+++ source.py
@@ -1,6 +1,7 @@
 import random
 import hashlib
-
+import tkinter
+from tkinter import *
 blocknum = 0;
 
 class Block:
@@ -13,7 +14,7 @@ class Block:
         self.nonce = 0;
         total_string = str(self.blocknum) + str(self.nonce) + self.data + self.previous_Hash;
         hash_value = hashlib.sha256(total_string.encode('utf-8')).hexdigest()
-        while (hash_value[0:5] != "12345"):
+        while (hash_value[0:4] != "1234"):
             self.nonce += 1
             total_string = str(self.blocknum) + str(self.nonce) + self.data + self.previous_Hash;
             hash_value = hashlib.sha256(total_string.encode('utf-8')).hexdigest()
@@ -27,6 +28,7 @@ class Block:
         if(hash_value != self.current_Hash):
             return False
         return True
+
 
 
 
@@ -46,6 +48,11 @@ print(Block1.blocknum, Block1.nonce, Block1.data, Block1.previous_Hash, Block1.c
 print(Block1.check_Self())
 print(Block2.blocknum, Block2.nonce, Block2.data, Block2.previous_Hash, Block2.current_Hash)
 print(Block3.blocknum, Block3.nonce, Block3.data, Block3.previous_Hash, Block3.current_Hash)
+
+top = tkinter.Tk();
+submit = Entry(top, bd = 5)
+submit.pack(side = RIGHT)
+top.mainloop();
 
 
 
